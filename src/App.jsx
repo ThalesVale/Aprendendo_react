fe8d5fa77751602182c4f95e6c1bfcc2edb46269
THALES VALE DA SILVA <25162457@edu119.local>
2026-02-27 08:35:52 -0300

    hjvjbhb

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <body>
+      <main>
+        <h1>Bem-vindo de volta!</h1>
+        <p>Digite suas credenciais para acessar o sistema</p>
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha" />
+        <button>Entrar no sistema</button>
+      </main>
+    </body>
+  );
+}
+
+export default App;
